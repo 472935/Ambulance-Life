62a26ecb6d02d0d3d2fa250de709d2cd6048225d
raulp <raulpgasch@gmail.com>
2023-11-30 21:52:51 +0100

    import constraint

--- CSPParking.py
+++ CSPParking.py
@@ -1,7 +1,7 @@
 from typing import Tuple
 from typing import List
 import sys
-from constraint import ∗
+from constraint import *
 
 def read_file(file_name: str) -> Tuple[str, str, List[str]]:
     file_input = open(file_name, "r")
@@ -22,7 +22,7 @@ def read_file(file_name: str) -> Tuple[str, str, List[str]]:
     return return_dimensions, return_electrical_connection, return_ambulances
 
 
-def arg_parse(arg_dimensions: str, arg_electric: str) -> Tuple[list, list]:
+def arg_parse(arg_dimensions: str, arg_electric: str, arg_ambulances:list) -> Tuple[list, list,list]:
     # format the dimensions into a list of m and n
     arg_dimensions = arg_dimensions.split("x")
     # remove the \n
@@ -31,7 +31,9 @@ def arg_parse(arg_dimensions: str, arg_electric: str) -> Tuple[list, list]:
     arg_electric = arg_electric.split(" ")
     arg_electric.pop(0)
     arg_electric = [[int(i[3]), int(i[1])] for i in arg_electric]
-    return arg_dimensions, arg_electric
+    arg_ambulances = [i.rstrip() for i in arg_ambulances]
+
+    return arg_dimensions, arg_electric,arg_ambulances
 
 class CSP:
     def __init__(self, variable, domain, constraint):
@@ -43,25 +45,29 @@ class CSP:
         return self.variables
 
 class grid:
+
     def __init__(self, n, m, electric_values):
         # m is the number of rows
         #n is the number of columns
         self.n = n
         self.m = m
         self.grid = [[' ' for _ in range(n)] for _ in range(m)]
-        for i in electric_values:
-            self.grid[i[1]-1][i[0]-1] = 'E'
+        for grid_elect_position in electric_values:
+            self.grid[grid_elect_position[1]-1][grid_elect_position[0]-1] = 'E'
 
     def __getgrid__(self):
         return self.grid
-
+    def getn(self):
+        return self.n
+    def getm(self):
+        return self.m
 
 parking_file_path = sys.argv[1]
 str_dimensions, str_electric, str_ambulances = read_file(parking_file_path)
 print(str_ambulances)
 # format
 print(str_electric)
-dimensions, electric = arg_parse(str_dimensions, str_electric)
+dimensions, electric,ambulances = arg_parse(str_dimensions, str_electric,str_ambulances)
 print(dimensions)
 print(electric)
 g = grid(dimensions[0], dimensions[1], electric)
@@ -69,5 +75,8 @@ string_grid = g.__getgrid__()
 print(string_grid)
 print(string_grid[0][0])
 print(string_grid[4][1])
+print(ambulances)
 
-
+problem = Problem ()
+for i in range(len(ambulances)):
+    problem.addVariable(ambulances[i],range(grid.getn()*grid.getm())) #We now consider the problem space of the grid to be a singular array of integers which (1,1) will correspond to 1 and (1,2) to 2 so on (2,1) to 6 in our case
