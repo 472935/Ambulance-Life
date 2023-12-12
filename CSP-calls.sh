5257887599a242cfd20a6d8c143933935d82fbc3
raulp <raulpgasch@gmail.com>
2023-12-12 23:02:00 +0100

    Test Parkings

---
+++ CSP-calls.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+python CSPParking.py ./CSP-tests/parking01
+python CSPParking.py ./CSP-tests/parking02
+python CSPParking.py ./CSP-tests/parking03
+python CSPParking.py ./CSP-tests/parking04
+python CSPParking.py ./CSP-tests/parking05
+python CSPParking.py ./CSP-tests/parking06
+python CSPParking.py ./CSP-tests/parking07
